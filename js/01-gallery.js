41f2ab7d64add56195e20c18a2164c680e6ac531
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-06-25 21:56:49 +0300

    added files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const collection = document.querySelector('.gallery');
+
+const listItems = galleryItems.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+).join('');
+
+collection.insertAdjacentHTML('afterbegin', listItems);
+
+collection.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const largeImageUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${largeImageUrl}" />`).show();
+});
